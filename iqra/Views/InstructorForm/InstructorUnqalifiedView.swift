2596b56de722e071b7879d6e83291714069175ef
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2026-06-06 14:37:03 -0400

    Pop back to root of navigation

--- iqra/Views/InstructorForm/InstructorUnqalifiedView.swift
+++ iqra/Views/InstructorForm/InstructorUnqalifiedView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct InstructorUnqalifiedView: View {
+    
+    @Environment(Router.self) private var router
+    
     var body: some View {
-        Text("Sorry, you are not yet \n qualified to be an instructor")
-            .multilineTextAlignment(.center)
+        VStack(spacing: 30) {
+            Text("Sorry, you are not yet \n qualified to be an instructor")
+                .multilineTextAlignment(.center)
+            
+            Button("Back to start") {
+                router.popToRoot()
+            }
+        }
     }
 }
 
 #Preview {
     InstructorUnqalifiedView()
+        .environment(Router())
 }
