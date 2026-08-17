471bf82d2fae88a6da6d2f26424e4033eac44747
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2026-08-17 15:19:15 -0400

    ZView

---
+++ iqra/Views/ZView.swift
@@ -0,0 +1,61 @@
+//
+//  ZView.swift
+//  iqra
+//
+//  Created by Ovi Hussain on 2026-08-17.
+//
+
+import SwiftUI
+
+struct ZView: View {
+    private let orangeSize: CGFloat = 100
+    @State private var orangePosition = CGPoint(x: 50, y: 50)
+    @State private var dragStart: CGPoint?
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.blue)
+
+            GeometryReader { geo in
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(width: orangeSize, height: orangeSize)
+                    .position(orangePosition)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                if dragStart == nil {
+                                    dragStart = orangePosition
+                                }
+                                let start = dragStart ?? orangePosition
+                                orangePosition = clampedPosition(
+                                    CGPoint(
+                                        x: start.x + value.translation.width,
+                                        y: start.y + value.translation.height
+                                    ),
+                                    in: geo.size
+                                )
+                            }
+                            .onEnded { _ in
+                                dragStart = nil
+                            }
+                    )
+            }
+            .border(Color.yellow, width: 2)
+        }
+        .ignoresSafeArea(.all)
+    }
+
+    private func clampedPosition(_ location: CGPoint, in size: CGSize) -> CGPoint {
+        let half = orangeSize / 2
+        return CGPoint(
+            x: min(max(location.x, half), size.width - half),
+            y: min(max(location.y, half), size.height - half)
+        )
+    }
+}
+
+#Preview {
+    ZView()
+}
